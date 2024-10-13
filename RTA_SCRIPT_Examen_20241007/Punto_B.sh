e780606d73c391dce709358755ec671ddc50e1df
Leoneldamianmejias <leocarp277@gmail.com>
2024-10-13 20:49:58 +0000

    Punto B: (Particionamiento y Montaje)

--- RTA_SCRIPT_Examen_20241007/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_B.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+# Variable usuario
+usuario=$(whoami)
 
 # Especificar el disco que vas a particionar
 DISCO="/dev/sdc"
 
 # Desmontar las particiones existentes
-echo "Desmontar particiones existentes"
+echo "Desmontando particiones existentes"
 sudo umount ${DISCO}* 2>/dev/null
 
 # Borrar sistemas de archivos existentes
 echo "Borrando sistemas de archivos en $DISCO"
 sudo wipefs -a $DISCO
 
-# Eliminar todas las particiones existentes en el disco (si es necesario)
+# Eliminar todas las particiones existentes en el disco
 echo "Eliminando todas las particiones existentes en $DISCO"
 sudo sfdisk --delete $DISCO
 
-# Crear las 11 particiones de igual tamaño
+# Crear particiones de igual tamaño
 echo "Creando particiones de igual tamaño en $DISCO"
 sudo fdisk $DISCO <<EOF
 n
@@ -40,34 +42,37 @@ e
 
 n
 l
+5
 
 +909M
 n
 l
+6
 
 +909M
 n
 l
+7
 
 +909M
 n
 l
+8
 
 +909M
 n
 l
+9
 
 +909M
 n
 l
+10
 
 +909M
 n
 l
-
-+909M
-n
-l
+11
 
 +909M
 
@@ -75,6 +80,8 @@ w
 EOF
 
 echo "Particiones creadas correctamente."
+
+sleep 2  # Un sleep para que el sistema se actualice bien.
 
 # Crear sistemas de archivos en las particiones
 echo "Formateando Particiones."
@@ -89,26 +96,25 @@ sudo mkfs.ext4 ${DISCO}9
 sudo mkfs.ext4 ${DISCO}10
 sudo mkfs.ext4 ${DISCO}11
 
-# Crear los puntos de montaje
-echo "Montando las particiones en las carpetas correspondientes..."
+sleep 2  # Otro sleep por las dudas.
 
 # Alumno 1
-sudo mount ${DISCO}1 ExamenesUTN/Alumno_1/parcial_1
-sudo mount ${DISCO}2 ExamenesUTN/Alumno_1/parcial_2
-sudo mount ${DISCO}3 ExamenesUTN/Alumno_1/parcial_3
+sudo mount ${DISCO}1 /home/${usuario}/Examenes-UTN/Alumno_1/parcial_1
+sudo mount ${DISCO}2 /home/${usuario}/Examenes-UTN/Alumno_1/parcial_2
+sudo mount ${DISCO}3 /home/${usuario}/Examenes-UTN/Alumno_1/parcial_3
 
 # Alumno 2
-sudo mount ${DISCO}5 ExamenesUTN/Alumno_2/parcial_1
-sudo mount ${DISCO}6 ExamenesUTN/Alumno_2/parcial_2
-sudo mount ${DISCO}7 ExamenesUTN/Alumno_2/parcial_3
+sudo mount ${DISCO}5 /home/${usuario}/Examenes-UTN/Alumno_2/parcial_1
+sudo mount ${DISCO}6 /home/${usuario}/Examenes-UTN/Alumno_2/parcial_2
+sudo mount ${DISCO}7 /home/${usuario}/Examenes-UTN/Alumno_2/parcial_3
 
 # Alumno 3
-sudo mount ${DISCO}8 ExamenesUTN/Alumno_3/parcial_1
-sudo mount ${DISCO}9 ExamenesUTN/Alumno_3/parcial_2
-sudo mount ${DISCO}10 ExamenesUTN/Alumno_3/parcial_3
+sudo mount ${DISCO}8 /home/${usuario}/Examenes-UTN/Alumno_3/parcial_1
+sudo mount ${DISCO}9 /home/${usuario}/Examenes-UTN/Alumno_3/parcial_2
+sudo mount ${DISCO}10 /home/${usuario}/Examenes-UTN/Alumno_3/parcial_3
 
 # Profesores
-sudo mount ${DISCO}11 ExamenesUTN/Profesores
+sudo mount ${DISCO}11 /home/${usuario}/Examenes-UTN/Profesores
 
 echo "Particiones montadas correctamente."
 
